e0d6e780ae96d31eefb550febf1c7afcc64000ba
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 19:53:14 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(array)
+  count = 0
+  newArray = []
+  while count < array.length do
+    yield array[count]
+    count += 1
+  end
+  return newArray
+end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+collection = ['ruby', 'javascript', 'python', 'objective-c']
 
+my_collect(collection) do |lang|
+  newArray.push(lang.upcase)
+end
+
+my_collect(array) do |name|
+  newArray.push(name.split(" ").first)
+end
